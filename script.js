d25d567d8eebdeb4dd73c4e4e1019dcb7e7382a6
Eduardo Sosa <edu_33@live.com.ar>
2021-09-18 22:59:30 -0300

    finished functionality

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const results = document.querySelector("results");
+    const results = document.querySelector(".results");
     const result = document.createElement("li");
 
     if(playerSelection == computerSelection){
@@ -48,6 +48,20 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function restart(){
+    playerWins = 0;
+    cpuWins = 0;
+    const finalResult = document.querySelector(".final-result");
+    finalResult.remove();
+    const playAgain = document.querySelector(".play-again");
+    playAgain.remove();
+    const results = document.createElement("ul");
+    results.classList.add("results");
+    showResults.appendChild(results);    
+    score.textContent = "0 - 0";
+}
+
+
 function game(playerPlay) {
     let roundWinner = playRound(playerPlay, computerPlay());
 
@@ -57,9 +71,16 @@ function game(playerPlay) {
         cpuWins++;
     }
 
+    score.textContent = playerWins + " - " + cpuWins;
+
     if(playerWins == 5 || cpuWins == 5){
+        const results = document.querySelector(".results");
         results.remove();
         const finalResult = document.createElement("h1");
+        finalResult.classList.add("final-result");
+        const playAgain = document.createElement("button");
+        playAgain.classList.add("play-again");
+
         if (playerWins == 5) {        
             finalResult.textContent = "You won!";
         }else if (cpuWins == 5) {
@@ -67,8 +88,12 @@ function game(playerPlay) {
       }
 
       showResults.appendChild(finalResult);
+      showResults.appendChild(playAgain);
+
+      playAgain.addEventListener("click", () => restart());
     }
 }
+
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
@@ -76,9 +101,13 @@ const scissors = document.querySelector("#scissors");
 let playerWins = 0;
 let cpuWins = 0;
 
-const showResults = document.querySelector(".show-results")
-const results = document.createElement("results");
-results.classList.add(".results")
+const showResults = document.querySelector(".show-results");
+const score = document.createElement("p");
+score.textContent = playerWins + " - " + cpuWins;
+score.classList.add(".score");
+showResults.appendChild(score);
+const results = document.createElement("ul");
+results.classList.add("results");
 showResults.appendChild(results);
 
 rock.addEventListener("click", () => game("rock"));
